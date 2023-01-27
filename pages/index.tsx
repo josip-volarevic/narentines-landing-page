d80317ea915e2acfeba3926e6cecea176f8ba944
josip-volarevic <josip.volarevic4@gmail.com>
2023-01-27 12:35:09 +0100

    chore: remove deprecated sections

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,53 +1,12 @@
 import type { NextPage } from 'next'
-import { Box } from '@mui/material'
-import CollectionShowcase from 'components/CollectionShowcase'
 import Header from 'components/layout/Header'
 import Footer from 'components/layout/Footer'
-import Main from 'components/layout/Main'
-import Section from 'components/layout/Section'
-import AccordionList from 'components/AccordionList'
-import ArticleList from 'components/ArticleList'
-import AvatarList from 'components/AvatarList'
-import { articleItems } from 'data'
-import { attributes as team } from 'content/team.md'
-import { attributes as faq } from 'content/faq.md'
-import { attributes as collection } from 'content/collection.md'
 // import { attributes as articles } from 'content/articles.md'
-import Image from 'next/future/image'
 
 const Home: NextPage = () => {
 	return (
 		<>
 			<Header />
-
-			<Main>
-				{articleItems ? (
-					<Section id='articles'>
-						<ArticleList items={articleItems} />
-					</Section>
-				) : null}
-
-				{collection ? (
-					<Section id='showcase'>
-						<CollectionShowcase collection={collection} />
-					</Section>
-				) : null}
-
-				{faq ? (
-					<Section id='faq' title={faq.title}>
-						<Box display='flex'>
-							<AccordionList items={faq.items} mr={{ md: 4 }} />
-							<Image src={faq.image} alt='' className='faq-image' height={680} width={360} />
-						</Box>
-					</Section>
-				) : null}
-
-				{team ? (
-					<Section id='team' title={team.title}>
-						<AvatarList items={team.members} />
-					</Section>
-				) : null}
-			</Main>
 
 			<Footer />
 		</>
